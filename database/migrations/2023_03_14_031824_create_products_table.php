ac4d96fe41e1852bf0e6426ac6837efe9e2418d8
MulyadiArman <mulyadiarman18@gmail.com>
2023-03-14 11:49:14 +0800

    test

---
+++ database/migrations/2023_03_14_031824_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('slug');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('description');
+            $table->integer('weight');
+            $table->bigInteger('c');
+            $table->integer('stock');
+            $table->integer('discount');
+            $table->timestamps();
+
+            // menghubungkan product ke tabel kategori
+            $table->foreign('category_id')->references('id')->on('categories');
+            // menghubungkan product ke tabel users
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
